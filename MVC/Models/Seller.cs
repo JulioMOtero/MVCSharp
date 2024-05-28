47d604907ea1fb5e142c89dffdc29b79b4628194
julio <julio@DESKTOP>
2024-05-28 18:28:21 -0300

    ForeignKey not Null (integridade referencial)

--- MVC/Models/Seller.cs
+++ MVC/Models/Seller.cs
@@ -9,7 +9,8 @@ namespace MVC.Models
         public string Email { get; set; }
         public DateTime Birthday { get; set; }
         public Double BaseSalary { get; set; }
-        public Department Department { get; set; }  
+        public Department Department { get; set; }
+        public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
