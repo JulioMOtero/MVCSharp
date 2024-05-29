0c6afe68690e2bb2040cdecbf847ae557e03126c
julio <julio@DESKTOP>
2024-05-29 16:17:03 -0300

    Delete Seller

--- MVC/Controllers/SellersController.cs
+++ MVC/Controllers/SellersController.cs
@@ -40,5 +40,29 @@ namespace MVC.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }
